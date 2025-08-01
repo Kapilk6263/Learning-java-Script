bca8ad88207ad6828f873eb498c42075b13994ac
Kapilk6263 <Kapilkapil6465@gmail.com>
2025-08-01 23:36:01 +0530

    pratice questions

---
+++ Assignment on function and array/Question1.js
@@ -0,0 +1,5 @@
+
+let arr=[23,44,22,45,66];
+
+let k=arr.reduce((sub,element)=>sub+=element,0)
+console.log(k);
---
+++ Assignment on function and array/Question2.js
@@ -0,0 +1,21 @@
+//write a js prog to find out the maximum value inside the array 
+
+let arr=[23,44,65,12,34];
+let max=arr[0];
+let k=arr.map((element)=> {
+   if(element>max){
+    max=element;
+   }
+})
+console.log(max);
+
+
+//minimum element 
+let min=arr[0];
+let kk=arr.map((element)=>{
+    if(element<min){
+        min=element;
+    }
+})
+console.log(min);
+
